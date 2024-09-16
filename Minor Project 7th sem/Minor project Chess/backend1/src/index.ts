893b6446bf159ecba8d489409f895f1d4f64164e
Ananya Trinach <ananyasharma8983@gmail.com>
2024-09-16 23:24:41 +0530

    first commit

--- Minor Project 7th sem/Minor project Chess/backend1/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { WebSocketServer } from 'ws'
-import { GameManager } from './gamemanager';
-
-const wss = new WebSocketServer({ port: 8080 });
-const gamemanager=new GameManager();
-wss.on('connection', function connection(ws) {
-  gamemanager.addUser(ws)
-  ws.on("disconnect",()=>gamemanager.removeUser(ws))
-});
